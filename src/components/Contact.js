58f8e5cbd87baa05c574f947d22f054997a89610
anu radha <anu.sivasubramanian@gmail.com>
2020-02-04 14:33:05 +0100

    solution to quest 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Contact.css";
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img src={props.avatar} className="avatar" alt={props.name} />
-      <div>
-        <h4 className="name">{props.name}</h4>
-        <div className="status">
-          <div
-            className={props.online ? "status-online" : "status-offline"}
-          ></div>
-          <p className="status-text">{props.online ? "online" : "offline"}</p>
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: this.props.online
+    };
+  }
+
+  render() {
+    const eventForOnClick = () => {
+      const newstatus = !this.state.status;
+      this.setState({ status: newstatus });
+    };
+    return (
+      <div className="Contact">
+        <img src={this.props.avatar} className="avatar" alt={this.props.name} />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div className="status">
+            <div
+              className={this.state.status ? "status-online" : "status-offline"}
+              onClick={eventForOnClick}
+            ></div>
+            <p className="status-text">
+              {this.props.online ? "online" : "offline"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default Contact;
